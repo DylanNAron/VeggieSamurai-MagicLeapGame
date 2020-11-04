79426ba5681a037265c7820399f029ac5044e065
DylanNAron <arondylan72@gmail.com>
2020-11-04 13:18:57 -0500

    grabswordstartspawners

--- Assets/Scipts/SwordGrab.cs
+++ Assets/Scipts/SwordGrab.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class SwordGrab : MonoBehaviour
 {
-
+	public GameObject grabSwordUI;
 	public GameObject controller;
+	private GameObject[] spawners;
 
 	void OnTriggerEnter(Collider collider) {
 
@@ -13,8 +14,21 @@ public class SwordGrab : MonoBehaviour
 			//attach
 			Debug.Log("Sword Grabbed");
 			collider.gameObject.transform.parent = gameObject.transform;
-			collider.gameObject.transform.rotation = gameObject.transform.rotation * Quaternion.Euler(0, 180, 0);
+			collider.gameObject.transform.rotation = gameObject.transform.rotation * Quaternion.Euler(0, 0, 0);
 			//Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+
+
+
+			//Disable UI
+			grabSwordUI.SetActive(false);
+
+
+			//Start Spawners
+			spawners = GameObject.FindGameObjectsWithTag("Spawners");
+			foreach(GameObject spawner in spawners)
+            {
+				spawner.SetActive(true);
+            }
 		}
 	}
 
